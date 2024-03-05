b39a3cdcec933c49afa739c91018489558a484e2
Caspar von Buchwaldt <casparvonbuchwaldt@gmail.com>
2024-03-05 18:23:36 +0100

    Almost done with displaying one question answer pair at a time

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,21 +1,17 @@
 class AnswersController < ApplicationController
   def create
-
     @answer = Answer.new(answer_params)
-    raise
-
     @answer.interview_question = InterviewQuestion.find(params[:interview_question_id])
-    raise
     if @answer.save
       redirect_to interview_path(@answer.interview_question.interview)
     else
-      render 'interviews/show'
+      render 'interviews/show', status: :unprocessable_entity
     end
   end
 
   private
 
   def answer_params
-    params.require(:answer).permit(:content, :interview_question_id)
+    params.require(:answer).permit(:content)
   end
 end
