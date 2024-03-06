df5bdca2a6356f1d20b6f1cc2ed880c6ec7814b5
Maximilian Braun <maxbraunbm@gmail.com>
2024-03-06 14:23:12 +0100

    api part started

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_05_101250) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_06_114637) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_05_101250) do
     t.bigint "question_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "answer_feedback"
     t.index ["interview_id"], name: "index_interview_questions_on_interview_id"
     t.index ["question_id"], name: "index_interview_questions_on_question_id"
   end
