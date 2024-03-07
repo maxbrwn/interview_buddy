5137d5491dfa01f252d201ea401f3d1d4e4992ab
Maximilian Braun <maxbraunbm@gmail.com>
2024-03-07 15:38:34 +0100

    enable disable buttons

---
+++ app/javascript/controllers/interview_buttons_controller.js
@@ -0,0 +1,23 @@
+import { Controller } from "@hotwired/stimulus"
+
+
+export default class extends Controller {
+  static targets = ["enable", "disable"]
+  static values = {
+    currentIndex: Number,
+    arraySize: Number
+  }
+  connect(){
+    const currentIndex = this.currentIndexValue;
+    const arraySize = this.arraySizeValue;
+
+
+    if (currentIndex === arraySize) {
+      this.enableTarget.classList.remove("d-none");
+    }
+
+    if (currentIndex === arraySize) {
+    this.disableTarget.classList.add("d-none");
+    }
+  }
+}
