b5787bc99c0ebb1689daf29f35e34f39d71ff872
Supriya Kadam <kadamsupriya900@gmail.com>
2024-03-08 14:43:37 +0100

    profile page showing the feedback

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -44,6 +44,12 @@ class InterviewsController < ApplicationController
     @answers = @interview.answers.pluck(:content)
   end
 
+  def my_profile
+    # get access to all the interviews of the current user
+    @user_interviews = current_user.interviews
+    # get access to the overall feedback of each interview
+  end
+
   private
 
   def set_interview
